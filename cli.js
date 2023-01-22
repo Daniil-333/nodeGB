e4e3bb49b1abcd278cdbb4b72c97023b943eb175
daniel333 <monobootica@yandex.ru>
2023-01-22 17:21:19 +0500

    dz4 is failed

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
-#!C:\Program Files\nodejs\node
+#!/usr/local/bin/node
 
-const readline = require('readline');
+const yargs = require('yargs');
+// const readline = require('readline');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
+const {hideBin} = require("yargs/helpers");
 
 
 /** 
@@ -15,45 +17,19 @@ const path = require('path');
  * 3. Во время чтения файлов искать в них заданную строку или паттерн. 
 */
 
-// const options = yargs
-//     .usage('Usage: -p <file path>')
-//     .option('p', {
-//         alias: 'path',
-//         describe: 'File path',
-//         type: 'string',
-//         demandOption: true,
-//     }).argv;
-//
-// console.log(options)
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
-
-// rl.question('Введите путь до файла: ', (filePath) => {
-//     console.log(filePath);
-//     rl.question('Введите кодировку файла: ', (encoding) => {
-//         console.log(encoding);
-//         rl.close();
-//     });
-// });
-//
-// const question = (query) => new Promise(resolve => rl.question(query, resolve));
-//
-// (async () => {
-//     const filePath = await question('Введите путь до файла: ');
-//     const encoding = await question('Введите кодировку файла: ');
-//
-//     const fullPath = path.join(__dirname, filePath);
-//     const data = await fs.readFile(fullPath, encoding);
-//
-//     console.log(data);
-//     rl.close();
-// })();
 const executionDir = process.cwd();
+const options = yargs
+    .usage('Usage: -p <file path>')
+    .option('p', {
+        alias: 'path',
+        describe: 'Directory path',
+        type: 'string' || undefined,
+        demandOption: false,
+    }).argv;
+const pathDir = path.resolve(options.p) ?? './';
+
 const isFile = (fileName) => fs.lstatSync(fileName).isFile();
-const list = fs.readdirSync('./').filter(isFile);
+const list = fs.readdirSync( pathDir ).filter(isFile);
 
 inquirer.prompt([
     {
@@ -63,6 +39,7 @@ inquirer.prompt([
         choices: list,
     }
 ]).then(({ fileName }) => {
+    console.log(argv.p)
     const fullPath = path.join(executionDir, fileName);
     const data = fs.readFileSync(fullPath, 'utf-8');
 
